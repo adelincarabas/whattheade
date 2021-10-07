b844840176bf8b7f4b509e75bc94fb6655291f65
adelincarabas <adelinromana@gmail.com>
2021-10-07 15:58:48 +0300

    finished styling the homepage not really happy with the colors

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,35 +1,45 @@
 import React from "react";
 
+import "./homepage.styles.scss";
+
 const HomePage = () => (
   <div className="homepage">
     <div className="directory-menu">
       <div className="menu-item">
         <div className="content">
-          <h1 className="title">HATS</h1>
+          <h1 className="title">Hats</h1>
           <span className="subtitle">SHOP NOW</span>
         </div>
       </div>
       <div className="menu-item">
         <div className="content">
-          <h1 className="title">JACKETS</h1>
+          <h1 className="title">Jackets</h1>
           <span className="subtitle">SHOP NOW</span>
         </div>
       </div>
       <div className="menu-item">
         <div className="content">
-          <h1 className="title">SNEAKERS</h1>
+          <h1 className="title">Sneakers</h1>
           <span className="subtitle">SHOP NOW</span>
         </div>
       </div>
       <div className="menu-item">
         <div className="content">
-          <h1 className="title">WOMENS</h1>
+          <h1 className="title">Women</h1>
           <span className="subtitle">SHOP NOW</span>
         </div>
       </div>
       <div className="menu-item">
         <div className="content">
-          <h1 className="title">MENS</h1>
+          <h1 className="title">Men</h1>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+    </div>
+    <div className="directory-menu-info">
+      <div className="menu-item-info">
+        <div className="content-info">
+          <h1 className="title">INFO</h1>
           <span className="subtitle">SHOP NOW</span>
         </div>
       </div>
