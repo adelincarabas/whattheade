e1b37727639a31eee52551375c998ffe66a35546
adelincarabas <adelinromana@gmail.com>
2021-10-07 14:31:43 +0300

    started the bone of the homepage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
 import "./App.css";
 
+import HomePage from "./pages/homepage/homepage.component.jsx";
+
 function App() {
-  return <div>Hello world</div>;
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
 }
 
 export default App;
